55750374584bfa63415233a1918f4c5f7bc75c79
physphil <physphil@gmail.com>
2015-08-10 22:41:49 -0400

    added ConversionType enum

---
+++ app/src/main/java/com/physphil/android/unitconverterultimate/models/ConversionType.java
@@ -0,0 +1,23 @@
+package com.physphil.android.unitconverterultimate.models;
+
+/**
+ * Enum of available conversion types
+ * Created by Phizz on 15-08-10.
+ */
+public enum ConversionType
+{
+    AREA,
+    COOKING,
+    DIGITAL_STORAGE,
+    ENERGY,
+    FUEL_CONSUMPTION,
+    LENGTH,
+    MASS,
+    POWER,
+    PRESSURE,
+    SPEED,
+    TEMPERATURE,
+    TIME,
+    TORQUE,
+    VOLUME
+}
