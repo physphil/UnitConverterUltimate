8d26eb6c542fbbf5fa144cce060a95262b175803
physphil <physphil@gmail.com>
2015-08-10 20:27:55 -0400

    finalized area tests

--- app/src/test/java/com/physphil/android/unitconverterultimate/test/ConversionTests.java
+++ app/src/test/java/com/physphil/android/unitconverterultimate/test/ConversionTests.java
@@ -21,7 +21,8 @@ public class ConversionTests
 {
     @Mock ConversionPresenter.ConversionView view;
 
-    private final double DOUBLE_DELTA = 0.0001;
+    private final double DELTA_4 = 0.0001;
+    private final double DELTA_10 = 0.0000000001;
     private Conversions mConversions;
     private ConversionPresenter mPresenter;
 
@@ -38,42 +39,24 @@ public class ConversionTests
     {
         Conversion area = mConversions.getById(Conversions.AREA);
 
-        // Test fromBase values
-        mPresenter.convert(5.5, area.getUnitById(0), area.getUnitById(0));
-        verify(view).showResult(eq(5.5));
+        // Test each fromBase and toBase value
         mPresenter.convert(5.5, area.getUnitById(0), area.getUnitById(1));
         verify(view).showResult(eq(5500000.0));
-        mPresenter.convert(5.5, area.getUnitById(0), area.getUnitById(2));
-        verify(view).showResult(eq(55000000000.0));
-        mPresenter.convert(5.5, area.getUnitById(0), area.getUnitById(3));
-        verify(view).showResult(eq(550.0));
-        mPresenter.convert(5.5, area.getUnitById(0), area.getUnitById(4));
-        verify(view).showResult(AdditionalMatchers.eq(2.1236, DOUBLE_DELTA));
-        mPresenter.convert(5.5, area.getUnitById(0), area.getUnitById(5));
-        verify(view).showResult(AdditionalMatchers.eq(6577945.2546559, DOUBLE_DELTA));
-        mPresenter.convert(5.5, area.getUnitById(0), area.getUnitById(6));
-        verify(view).showResult(AdditionalMatchers.eq(59201507.291903, DOUBLE_DELTA));
-        mPresenter.convert(5.5, area.getUnitById(0), area.getUnitById(7));
-        verify(view).showResult(AdditionalMatchers.eq(8525017050.0341, DOUBLE_DELTA));
-        mPresenter.convert(5.5, area.getUnitById(0), area.getUnitById(8));
-        verify(view).showResult(AdditionalMatchers.eq(1359.0795980694, DOUBLE_DELTA));
-
-        // Test toBase values
-        mPresenter.convert(5.5, area.getUnitById(1), area.getUnitById(0));
-        verify(view).showResult(eq(0.0000055));
-        mPresenter.convert(5.5, area.getUnitById(2), area.getUnitById(0));
-        verify(view).showResult(eq(0.00000000055));
-        mPresenter.convert(5.5, area.getUnitById(3), area.getUnitById(0));
-        verify(view).showResult(eq(0.055));
-        mPresenter.convert(5.5, area.getUnitById(4), area.getUnitById(0));
-        verify(view).showResult(AdditionalMatchers.eq(14.244934606848, DOUBLE_DELTA));
-        mPresenter.convert(5.5, area.getUnitById(5), area.getUnitById(0));
-        verify(view).showResult(AdditionalMatchers.eq(0.00000459870048, DOUBLE_DELTA));
-        mPresenter.convert(5.5, area.getUnitById(6), area.getUnitById(0));
-        verify(view).showResult(AdditionalMatchers.eq(0.00000051096672, DOUBLE_DELTA));
-        mPresenter.convert(5.5, area.getUnitById(7), area.getUnitById(0));
-        verify(view).showResult(AdditionalMatchers.eq(0.00000000354838, DOUBLE_DELTA));
+        mPresenter.convert(5.5, area.getUnitById(1), area.getUnitById(2));
+        verify(view).showResult(eq(55000.0));
+        mPresenter.convert(5.5, area.getUnitById(2), area.getUnitById(3));
+        verify(view).showResult(eq(0.000000055));
+        mPresenter.convert(5.5, area.getUnitById(3), area.getUnitById(4));
+        verify(view).showResult(AdditionalMatchers.eq(0.0212, DELTA_4));
+        mPresenter.convert(5.5, area.getUnitById(4), area.getUnitById(5));
+        verify(view).showResult(eq(17036800.0));
+        mPresenter.convert(5.5, area.getUnitById(5), area.getUnitById(6));
+        verify(view).showResult(eq(49.5));
+        mPresenter.convert(5.5, area.getUnitById(6), area.getUnitById(7));
+        verify(view).showResult(eq(792.0));
+        mPresenter.convert(5.5, area.getUnitById(7), area.getUnitById(8));
+        verify(view).showResult(AdditionalMatchers.eq(0.0000008768, DELTA_10));
         mPresenter.convert(5.5, area.getUnitById(8), area.getUnitById(0));
-        verify(view).showResult(AdditionalMatchers.eq(0.0222577103232, DOUBLE_DELTA));
+        verify(view).showResult(AdditionalMatchers.eq(0.0223, DELTA_4));
     }
 }
