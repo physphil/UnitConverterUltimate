ed0399fb813491c7fa847cfbc196b2ac4be0b808
pshadlyn <physphil@gmail.com>
2015-08-19 16:45:17 -0400

    added apache license

--- app/src/main/java/com/physphil/android/unitconverterultimate/fragments/HelpDialogFragment.java
+++ app/src/main/java/com/physphil/android/unitconverterultimate/fragments/HelpDialogFragment.java
@@ -1,20 +1,28 @@
+/*
+ * Copyright 2015 Phil Shadlyn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.physphil.android.unitconverterultimate.fragments;
 
 import android.app.Dialog;
 import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
-import android.text.SpannableString;
-import android.text.method.LinkMovementMethod;
-import android.text.util.Linkify;
-import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.physphil.android.unitconverterultimate.Preferences;
