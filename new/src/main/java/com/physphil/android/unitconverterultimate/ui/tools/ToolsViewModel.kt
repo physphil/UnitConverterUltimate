3719e4199e5b1a6e56753a9732ff0eb5ddbfa166
Phil Shadlyn <phil.shadlyn@shopify.com>
2019-09-17 21:12:39 -0400

    setup new project module

---
+++ new/src/main/java/com/physphil/android/unitconverterultimate/ui/tools/ToolsViewModel.kt
@@ -0,0 +1,13 @@
+package com.physphil.android.unitconverterultimate.ui.tools
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ToolsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is tools Fragment"
+    }
+    val text: LiveData<String> = _text
+}
