c06f91081a4d1d5b7e72ef3605fe7e9e28c8a952
Phil Shadlyn <phil.shadlyn@shopify.com>
2019-12-01 17:40:30 -0500

    added initial temp calcs

--- v6000/src/main/java/com/physphil/android/unitconverterultimate/conversion/ConversionRepository.kt
+++ v6000/src/main/java/com/physphil/android/unitconverterultimate/conversion/ConversionRepository.kt
@@ -12,7 +12,7 @@ class ConversionRepository {
     fun convert(value: BigDecimal, initial: Unit, final: Unit): BigDecimal =
         when {
             initial is Area && final is Area -> value.convertStandard(initial, final)
-            initial is Temperature && final is Temperature -> TODO()
+            initial is Temperature && final is Temperature -> value.convertTemperature(initial, final)
             else -> throw IllegalArgumentException("The initial unit $initial and final unit $final are not of the same type, and cannot be converted.")
         }
 
@@ -21,4 +21,52 @@ class ConversionRepository {
         val multiplier = initial.toStandard * final.fromStandard
         return this * multiplier
     }
+
+    private fun BigDecimal.convertTemperature(initial: Temperature, final: Temperature): BigDecimal =
+        when (final) {
+            Temperature.Celsius -> toCelsius(initial)
+            Temperature.Fahrenheit -> toFahrenheit(initial)
+            Temperature.Kelvin -> TODO()
+            Temperature.Rankine -> TODO()
+            Temperature.Delisle -> TODO()
+            Temperature.Newton -> TODO()
+            Temperature.Reaumur -> TODO()
+            Temperature.Romer -> TODO()
+            Temperature.GasMark -> TODO()
+        }
+
+    private fun BigDecimal.toCelsius(initial: Temperature): BigDecimal =
+        when (initial) {
+            Temperature.Celsius -> this
+            Temperature.Fahrenheit -> (this - 32) * 5 / 9
+            Temperature.Kelvin -> this - 273.15
+            Temperature.Rankine -> (this - 491.67) * 5 / 9
+            Temperature.Delisle -> 100 - this * 2 / 3
+            Temperature.Newton -> this * 100 / 33
+            Temperature.Reaumur -> this * 5 / 4
+            Temperature.Romer -> (this - 7.5) * 40 / 21
+            Temperature.GasMark -> TODO()
+        }
+
+    private fun BigDecimal.toFahrenheit(initial: Temperature): BigDecimal =
+        when (initial) {
+            Temperature.Celsius -> this * 9 / 5 + 32
+            Temperature.Fahrenheit -> this
+            Temperature.Kelvin -> this * 9 / 5 - 459.67
+            Temperature.Rankine -> this - 459.6
+            Temperature.Delisle -> 212 - this * 6 / 5
+            Temperature.Newton -> this * 60 / 11 + 32
+            Temperature.Reaumur -> this * 9 / 4 + 32
+            Temperature.Romer -> (this - 7.5) * 24 / 7 + 32
+            Temperature.GasMark -> TODO()
+        }
+    
+    private operator fun BigDecimal.plus(i: Int): BigDecimal = this + i.toBigDecimal()
+    private operator fun BigDecimal.minus(i: Int): BigDecimal = this - i.toBigDecimal()
+    private operator fun BigDecimal.minus(d: Double): BigDecimal = this - d.toBigDecimal()
+    private operator fun BigDecimal.times(i: Int): BigDecimal = this * i.toBigDecimal()
+    private operator fun BigDecimal.div(i: Int): BigDecimal = this / i.toBigDecimal()
+
+    private operator fun Int.minus(bigDecimal: BigDecimal): BigDecimal = this.toBigDecimal() - bigDecimal
+    private operator fun Int.times(bigDecimal: BigDecimal): BigDecimal = this.toBigDecimal() * bigDecimal
 }
